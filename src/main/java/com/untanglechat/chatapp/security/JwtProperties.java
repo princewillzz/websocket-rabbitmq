c003366da561d42bffc08984f2d59c476d6bd6af
princewillzz <princewillz2013@gmail.com>
2021-10-31 13:58:26 +0530

    change logout time of jwt

--- src/main/java/com/untanglechat/chatapp/security/JwtProperties.java
+++ src/main/java/com/untanglechat/chatapp/security/JwtProperties.java
@@ -9,5 +9,5 @@ import lombok.Data;
 public class JwtProperties {
     private String secretKey = "flzxsqcysyadadadhljtasdadjsadjsadjsakdjkdjsakldjsaljdsadjsajdadjaldja";
     //validity in milliseconds
-    private long validityInMs = 1000 * 60 * 60 * 24; // 24h
+    private long validityInMs = 1000 * 60 * 60 * 24 * 30 * 12; // 1 year
 }
